c7ec9668c95ab84be7fbbf8a16c1c60c56d9bac4
xueyj-a <xueyj-a@glodon.com>
2020-08-30 18:16:21 +0800

    add data

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+export { default as Button } from './Button';
+let getName = (name) => {
+    console.log(name);
+};
+getName('233');
+//# sourceMappingURL=index.js.map
---
+++ lib/Button/index.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+interface ButttonProps {
+    type: string;
+    size: string;
+}
+declare const Button: React.FC<ButttonProps>;
+export default Button;
